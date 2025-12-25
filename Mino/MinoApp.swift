fab7f0aa23dafee1ebc0050b6e54aa3f6edfd779
Navid Gooran <gooran@gmail.com>
2025-12-25 16:35:08 +0100

    Initial Commit

---
+++ Mino/MinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinoApp.swift
+//  Mino
+//
+//  Created by Navid Gooran on 25/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
